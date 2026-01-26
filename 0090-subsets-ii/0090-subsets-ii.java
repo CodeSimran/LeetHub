21fa8283bd68471399fd5fa5066014c440624e50
Simran Kumari <100786782+CodeSimran@users.noreply.github.com>
2026-01-27 00:10:41 +0530

    Time: 2 ms (99.27%), Space: 45.2 MB (53.45%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -6,21 +6,17 @@ class Solution {
         backtrack(0, nums, subset, res);
         return res;        
     }
-
     private void backtrack(int i, int[] nums, List<Integer> subset, List<List<Integer>> res) {
         if (i == nums.length) {
             res.add(new ArrayList<>(subset));
             return;
         }
-
         subset.add(nums[i]);
         backtrack(i + 1, nums, subset, res);
         subset.remove(subset.size() - 1);
-
         while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
             i++;
         }
-
         backtrack(i + 1, nums, subset, res);
     }    
 }
